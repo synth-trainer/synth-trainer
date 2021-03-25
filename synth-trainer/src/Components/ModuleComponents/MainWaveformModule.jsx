197011841b07e79828ee9871da7b3db3a2b76799
David Sackler <davidsackler@Davids-MBP.home>
2021-03-25 00:15:58 -0700

    Having issues lining up grid components

--- synth-trainer/src/Components/ModuleComponents/MainWaveformModule.jsx
+++ synth-trainer/src/Components/ModuleComponents/MainWaveformModule.jsx
@@ -69,20 +69,20 @@ const MainWaveformModule = (props) => {
   if (moduleRef === undefined) {
     return <p>Loading</p>;
   }
+  
 
   return (
     <div>
-      <Card id="sign-in-card" className="text-center w-50 customCard">
+      <Card id="sign-in-card" className="text-center w-50 customCard" alignItems="center">
         <Card.Title id="sign-in-label">Intro: Basic Waveforms</Card.Title>
         <Card.Text>
           Oscillators are the fundamental building blocks of a synths. The word
           “oscillator” sounds complicated, but it’s a fancy word with a simple
           meaning. An oscillator creates a sound.
         </Card.Text>
-
-        <Grid container spacing={1}>
-          <Grid container item xs={12} spacing={3}>
-            <Grid item xs={4}>
+        <Card.Body>
+          <Grid item container direction="row" spacing={2} alignItems="center" className = "section">
+            <Grid item xs={12} sm={6} md={5}>
               <WaveformExample
                 waveform="sine"
                 message="A sine wave is the simplest waveform with no harmonics or overtones. 
@@ -92,84 +92,44 @@ const MainWaveformModule = (props) => {
                 buttonHandler={buttonHandler}
               />
             </Grid>
-          </Grid>
-          <Grid container item xs={12} spacing={3}>
-            <Grid item xs={4}>
+            <Grid item xs={12} sm={6} md={5}>
               <WaveformExample
-                waveform="sine"
-                message="A sine wave is the simplest waveform with no harmonics or overtones. 
-                        It generates a smooth, clean sound, much like the way the waveform looks."
+                waveform="triangle"
+                message="Rather than a smooth, curvy waveform, the triangle wave consists of
+                repeating upward and downward slopes that generate a slightly brighter tone than
+                sine waves."
                 volume={volume}
                 setVolume={setVolume}
                 buttonHandler={buttonHandler}
               />
             </Grid>
           </Grid>
-          <Grid container item xs={12} spacing={3}>
-            <Grid item xs={4}>
+          <br />
+          {/* in between */}
+          <Grid item container direction="row" spacing={2} alignItems="center" className = "section">
+            <Grid item xs={12} sm={6} md={5}>
               <WaveformExample
-                waveform="sine"
-                message="A sine wave is the simplest waveform with no harmonics or overtones. 
-                        It generates a smooth, clean sound, much like the way the waveform looks."
+                waveform="square"
+                message="A square wave generates a buzzier tone than a sine wave due to its
+              instant changes in amplitude. It introduces an important music term called harmonics."
+                volume={volume}
+                setVolume={setVolume}
+                buttonHandler={buttonHandler}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={5}>
+              <WaveformExample
+                waveform="sawtooth"
+                message="Sawtooth waves generate the richest tones of the 4 common waveforms.
+                Its waveform consists of linear rises followed by the instant amplitude change,
+                much like the square wave."
                 volume={volume}
                 setVolume={setVolume}
                 buttonHandler={buttonHandler}
               />
             </Grid>
           </Grid>
-        </Grid>
-
-
-
-
-        <Grid item container direction="row" spacing={2} alignItems="center">
-          <Grid item xs={12} sm={6} md={5}>
-            <WaveformExample
-              waveform="sine"
-              message="A sine wave is the simplest waveform with no harmonics or overtones. 
-                      It generates a smooth, clean sound, much like the way the waveform looks."
-              volume={volume}
-              setVolume={setVolume}
-              buttonHandler={buttonHandler}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} md={5}>
-            <WaveformExample
-              waveform="triangle"
-              message="Rather than a smooth, curvy waveform, the triangle wave consists of
-              repeating upward and downward slopes that generate a slightly brighter tone than
-              sine waves."
-              volume={volume}
-              setVolume={setVolume}
-              buttonHandler={buttonHandler}
-            />
-          </Grid>
-        </Grid>
-        <br />
-        {/* in between */}
-        <Grid item container direction="row" spacing={2} alignItems="center">
-          <Grid item xs={12} sm={6} md={5}>
-            <WaveformExample
-              waveform="square"
-              message="A square wave generates a buzzier tone than a sine wave due to its
-            instant changes in amplitude. It introduces harmonics."
-              volume={volume}
-              setVolume={setVolume}
-              buttonHandler={buttonHandler}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} md={5}>
-            <WaveformExample
-              waveform="sawtooth"
-              message="Sawtooth waves generate the richest tones of the 4 common waveforms.
-              Its waveform consists of linear rises followed by the instant amplitude change,
-              much like the square wave."
-              volume={volume}
-              setVolume={setVolume}
-              buttonHandler={buttonHandler}
-            />
-          </Grid>
-        </Grid>
+        </Card.Body>
         <br />
         <Button onClick={buttonHandler} name="next">
           Take the Test!
